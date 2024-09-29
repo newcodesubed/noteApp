a8c64a18fe3ff11d5cbb3c4e31bc549b946a898f
Subed <subedshahxi@gmail.com>
2024-09-29 17:18:58 +0545

    Remove dead code from ux

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const d = new Date();
-const year = d.getFullYear();
-
-function Footer(){
-    return (
-        <footer className="footer"><p>copyright{year} </p></footer>
-    )
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
+
 export default Footer;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Note(){
-    return(
-        <div className="note">
-            <h1>TITLE FOR YOUR NOTE</h1>
-            <p>THIS IS THE DESCRIPTION FOR YOUR NOTE</p>
-        </div>
-        
-    );
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the note title</h1>
+      <p>This is the note content</p>
+    </div>
+  );
 }
+
 export default Note;
